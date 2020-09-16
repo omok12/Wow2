bfe157713f5b2d8c023d434893b4a638abd1c1db
o <olivermok@gmail.com>
2020-09-15 17:46:22 -0700

    changed output name to datetime

--- api_to_s3.py
+++ api_to_s3.py
@@ -1,10 +1,13 @@
 import requests
 import json
 import gzip
+from datetime import datetime
+
+filename = datetime.now().strftime('%Y%m%d%H')
 
 r = requests.get('https://us.api.blizzard.com/data/wow/connected-realm/5/auctions?namespace=dynamic-us&locale=en_US&access_token=US7HcOymVW43TaOzGvctT7AlEJLjcH9NlK')
 json_data = json.dumps(r.json())
 encoded = json_data.encode('utf-8')
 compressed = gzip.compress(encoded)
-with gzip.open('testcompress.gz', 'wb') as f:
+with gzip.open(f'{filename}.gz', 'wb') as f:
     f.write(compressed)
