534ba735095174ac1fce64f9322949168a69f2b5
o <olivermok@gmail.com>
2020-09-15 17:00:30 -0700

    add json gzip compress

--- api_to_s3.py
+++ api_to_s3.py
@@ -1,4 +1,10 @@
 import requests
+import json
+import gzip
 
 r = requests.get('https://us.api.blizzard.com/data/wow/connected-realm/5/auctions?namespace=dynamic-us&locale=en_US&access_token=US7HcOymVW43TaOzGvctT7AlEJLjcH9NlK')
-print(r.json())
+json_data = json.dumps(r.json())
+encoded = json_data.encode('utf-8')
+compressed = gzip.compress(encoded)
+with gzip.open('testcompress.gz', 'wb') as f:
+    f.write(compressed)
