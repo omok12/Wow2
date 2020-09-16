e67a5dd1eba0218a179713468e4fd4b81419b37e
o <olivermok@gmail.com>
2020-09-15 20:16:45 -0700

    added infinite loop and sleep

--- api_to_s3.py
+++ api_to_s3.py
@@ -2,6 +2,7 @@ import requests
 import json
 import gzip
 from datetime import datetime
+import time
 
 def get_token(client_id, client_secret):
     data = {'grant_type': 'client_credentials'}
@@ -9,7 +10,7 @@ def get_token(client_id, client_secret):
     return r.json()['access_token']
 
 def api_to_gz(connected_realm_id, access_token):
-    filename = datetime.now().strftime('%Y%m%d%H')
+    filename = datetime.now().strftime('%Y%m%d%H%M')
     r = requests.get(f'https://us.api.blizzard.com/data/wow/connected-realm/{connected_realm_id}/auctions?namespace=dynamic-us&locale=en_US&access_token={access_token}')
     json_data = json.dumps(r.json())
     encoded = json_data.encode('utf-8')
@@ -18,9 +19,12 @@ def api_to_gz(connected_realm_id, access_token):
         f.write(compressed)
 
 if __name__ == '__main__':
-    try:
-        api_to_gz('5', access_token)
-    except:
-        access_token = get_token('41aa51073e5c4ef1a80682f2f4b24cec', 'Vx0dE7jjw7zPoekWjiI3fcNiYkAN6lxb')
-        api_to_gz('5', access_token)
+    while True:
+        try:
+            api_to_gz('5', access_token)
+        except:
+            access_token = get_token('41aa51073e5c4ef1a80682f2f4b24cec', 'Vx0dE7jjw7zPoekWjiI3fcNiYkAN6lxb')
+            api_to_gz('5', access_token)
+        time.sleep(60)
+
 
